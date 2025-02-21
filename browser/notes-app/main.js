70717e4f171a67d74fbd34f440b626e19065a3c2
Th. Androutsos <a8anassis@gmail.com>
2025-02-21 19:37:44 +0200

    feat: notes app created

--- browser/notes-app/main.js
+++ browser/notes-app/main.js
@@ -38,17 +38,30 @@ function printGRDate() {
   document.querySelector('#dateTxt').innerHTML = dateStr + '<br>' + timeStr
 }
 
+/**
+ * Controller in MVC.
+ * @param {Object} data 
+ * @returns nothing if data.note is empty or null
+ */
 function onInsertHandler(data) {
   if (!data.note) return
   insertNote(data)
   reset()
 } 
 
+/**
+ * Model function, inserts a new note.
+ * @param {Object} note 
+ */
 function insertNote(note) {
   notes = [...notes, note]
   count++
   renderNotes()
 }
+
+/*
+ *  View rerendering
+ */
 
 function renderNotes() {
   const container = document.getElementById('notesWrapper')
@@ -59,8 +72,35 @@ function renderNotes() {
         <label id="${'noteTxt' + note.key}" for="${'noteCheck' + note.key}" class="w-[200px] max-h-[150px] leading-[1.2rem] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500': ''}">${note.note}</label>
       </div>
       <button type="button" id="${'noteDelBtn' + note.key}" class="w-[35px] h-[35px]" onclick="deleteNote(${note.key})">X</button>
-  
-  ` )
+    </div>`).join("")
 }
+
+/*
+ *  Model functions
+ */
+function strikeThrough(key) {
+  notes = notes.map(note => 
+    note.key === key ? {...note, softDeleted: !note.softDeleted} : {...note}
+  )
+  renderNotes()
+}
+
+function deleteNote(key) {
+    notes = notes.filter(note => note.key !== key)
+    renderNotes()
+}
+
+/**
+ * View function
+ */
+function reset() {
+  document.querySelector('inputNote').value = ""
+}
+
+
+
+
+
+
 
 
